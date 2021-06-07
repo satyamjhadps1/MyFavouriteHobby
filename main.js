87407fabf8e7c6a7e0900385997b497cb20bb6af
satyamjhadps1 <84779436+satyamjhadps1@users.noreply.github.com>
2021-06-07 11:29:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function reasons(){
+    var reasons="I love coding. I like coding as it helps me make work easier with the help of computer. I even think that all we could'nt do anything if there was no code because in this pandemic we would'nt be able to do online whitehatjr classes, online class of school, my father could not work etc. It helps are mind get sharper as we will give our ideas to be more popular which makes our mind sharp. And also we would get nice expeience on the world of computers."; 
+    document.getElementById("para").innerHTML=reasons;
+}
